9c4973a256f841594e96bf9f95291d32def6a18f
Ana <94186357+anaemerubio@users.noreply.github.com>
2022-04-25 10:41:25 +0200

    Calculate Total [Ex 1 Complete]

--- sprint7/src/App.js
+++ sprint7/src/App.js
@@ -2,34 +2,59 @@ import React, {useState} from 'react';
 
 export default function App() {
 
-const [checkOne, setcheckOne] = useState(false);
-const [checkTwo, setcheckTwo] = useState(false);
-const [checkThree, setcheckThree] = useState(false);
+  const [checkOne, setcheckOne] = useState(false);
+  const [checkTwo, setcheckTwo] = useState(false);
+  const [checkThree, setcheckThree] = useState(false);
+  const [budget, setBudget] = useState(0);
 
-const handleChangeOne = () => setcheckOne(!checkOne);
-const handleChangeTwo = () => setcheckTwo(!checkTwo);
-const handleChangeThree = () => setcheckThree(!checkThree);
+  const handleOne = () => setcheckOne(!checkOne);
+  
+  const handleTwo = () => setcheckTwo(!checkTwo);
 
-return (
-
-  <div>
-    <Checkbox label='Web development (500€)' check={checkOne} onChange={handleChangeOne} value={500}/>
-    
-    <Checkbox label='Seo consulting (300€)' check={checkTwo} onChange={handleChangeTwo} value={300}/>
-
-    <Checkbox label='GoogleAdds Campaign (200€)' check={checkThree} onChange={handleChangeThree} value={200}/>
-  </div>
-
-);
-};
-
-const Checkbox = ({label, check, onChange, value}) => {
+  const handleThree = () => setcheckThree(!checkThree);
+  
+  function handleBudget(evt){
+    let serviceCost = Number(evt.target.value);
+    evt.target.checked ? setBudget(budget+serviceCost) : setBudget(budget-serviceCost)
+  }
 
   return (
+    <>
+    <div>
+      <Checkbox 
+        check={checkOne} 
+        onChange={handleOne}
+        onClick={handleBudget}
+        value={400}
+        label='Web development (500€)'
+      />
+      
+      <Checkbox 
+        check={checkTwo} 
+        onChange={handleTwo} 
+        onClick={handleBudget}
+        value={300}
+        label='Seo consulting (300€)'
+      />
+
+      <Checkbox 
+        check={checkThree} 
+        onChange={handleThree} 
+        onClick={handleBudget}
+        value={200}
+        label='GoogleAdds Campaign (200€)'
+      />
+    </div>
+  <p>Total: {budget}</p>
+  </>
+  );
+};
+
+const Checkbox = ({check, onChange, onClick, value,label}) => {
+  return (
     <label>
-      <input type='checkbox' checked={check} onChange={onChange} value={value}/>
+      <input type='checkbox' checked={check} onChange={onChange} onClick={onClick} value={value}/>
       {label}
     </label>
   );
-
 }
