fb2f1d6246c774d1ad7d35ca906484c7f873c61b
Ana <94186357+anaemerubio@users.noreply.github.com>
2022-04-25 11:08:48 +0200

    First Panel implemented

--- sprint7/src/App.js
+++ sprint7/src/App.js
@@ -1,60 +1,84 @@
 import React, {useState} from 'react';
+import styled from 'styled-components';
+
+const Panel = styled.div`
+  width: 200px;
+  height: 200px;
+  border: 1px solid black;
+`;
 
 export default function App() {
 
-  const [checkOne, setcheckOne] = useState(false);
-  const [checkTwo, setcheckTwo] = useState(false);
-  const [checkThree, setcheckThree] = useState(false);
-  const [budget, setBudget] = useState(0);
+  const [checkOne, setCheckOne] = useState(false);
+  const [checkTwo, setCheckTwo] = useState(false);
+  const [checkThree, setCheckThree] = useState(false);
+  const [inputOne, setInputOne] = useState(0);
+  const [inputTwo, setInputTwo] = useState(0);
+  const [total, setTotal] = useState(0);
 
-  const handleOne = () => setcheckOne(!checkOne);
-  
-  const handleTwo = () => setcheckTwo(!checkTwo);
+  const handleCheck1 = () => setCheckOne(!checkOne);
+  const handleCheck2 = () => setCheckTwo(!checkTwo);
+  const handleCheck3 = () => setCheckThree(!checkThree);
 
-  const handleThree = () => setcheckThree(!checkThree);
+  const handleInput1 = (e) => setInputOne(e.target.value);
+  const handleInput2 = (e) => setInputTwo(e.target.value);
   
-  function handleBudget(evt){
-    let serviceCost = Number(evt.target.value);
-    evt.target.checked ? setBudget(budget+serviceCost) : setBudget(budget-serviceCost)
+  function handleTotal(event){
+    let evtValue = Number(event.target.value);
+    (event.target.checked) ? setTotal(total+evtValue) : setTotal(total-evtValue)
   }
 
   return (
     <>
-    <div>
-      <Checkbox 
-        check={checkOne} 
-        onChange={handleOne}
-        onClick={handleBudget}
-        value={400}
-        label='Web development (500€)'
-      />
-      
-      <Checkbox 
-        check={checkTwo} 
-        onChange={handleTwo} 
-        onClick={handleBudget}
-        value={300}
-        label='Seo consulting (300€)'
-      />
+      <div>
+        <h3>How we can help you ? </h3>
+        <Checkbox 
+          check={checkOne} 
+          onChange={handleCheck1}
+          onClick={handleTotal}
+          value={500} 
+        >
+          Web development (500€)
+        </Checkbox>
+        {checkOne && 
+        <Panel>
+          <input type="number" value={inputOne} onChange={handleInput1}/>
+          <input type="number" value={inputTwo} onChange={handleInput2}/>
+        </Panel>}
+        <Checkbox 
+          check={checkTwo} 
+          onChange={handleCheck2} 
+          onClick={handleTotal}
+          value={300}
+        > 
+          Seo Analysis (300€)
+        </Checkbox>
 
-      <Checkbox 
-        check={checkThree} 
-        onChange={handleThree} 
-        onClick={handleBudget}
-        value={200}
-        label='GoogleAdds Campaign (200€)'
-      />
+        <Checkbox 
+          check={checkThree} 
+          onChange={handleCheck3} 
+          onClick={handleTotal}
+          value={200}
+        >
+          Google Adds campaign (200€)
+        </Checkbox>
     </div>
-  <p>Total: {budget}</p>
+    <p>Total price is {total}</p>
   </>
   );
 };
 
-const Checkbox = ({check, onChange, onClick, value,label}) => {
+const Checkbox = ({check, onChange, onClick, value, children}) => {
   return (
     <label>
-      <input type='checkbox' checked={check} onChange={onChange} onClick={onClick} value={value}/>
-      {label}
+      <input 
+        type='checkbox' 
+        checked={check} 
+        onChange={onChange} 
+        onClick={onClick} 
+        value={value}
+      />
+      {children}
     </label>
   );
 }
